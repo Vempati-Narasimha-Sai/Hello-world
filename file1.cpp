77b62c1396fa2182c2cc430500900437482a5134
Narasimha Sai Vempati <narasimhasai07@gmail.com>
2021-06-10 16:40:48 +0530

    added second line too

--- file1.cpp
+++ file1.cpp
@@ -5,6 +5,8 @@ int main() {
 	// this is a comment
 	// this is new addition to file
 	// this is a second change that is written to the file
+	// first line before commit
+	// added second line after commit
 	printf("this is file 1\n");
 	return 0;
 }
